9d9673ff6ac2473b16991c2be3be34d164b6ff5d
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:33:10 +0700

    Update [code].js

--- api/[code].js
+++ api/[code].js
@@ -1,10 +1,13 @@
-// simpan sebagai [code].js atau pages/[code].js
-// menangani redirect dari short code ke URL asli
+import fs from 'fs';
+import path from 'path';
 
-import { store } from './shorten';   // impor store yang sama
+const DB_PATH = path.resolve('./db.json');
+
 export default function handler(req, res) {
   const { code } = req.query;
-  const longUrl = store.get(code);
+  const db = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8'));
+  const longUrl = db[code];
+
   if (!longUrl) return res.status(404).send('Not found');
   res.redirect(301, longUrl);
 }
