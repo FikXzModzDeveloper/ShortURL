5f9d30c77fe0739b84c23df29536454029b26584
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:12:09 +0700

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -1,31 +1,25 @@
 import { nanoid } from 'nanoid';
-import fs from 'fs';
+import fs   from 'fs';
 import path from 'path';
 
 const DB = path.join(process.cwd(), 'db.json');
 
-function readDB() {
-  try {
-    return JSON.parse(fs.readFileSync(DB, 'utf8'));
-  } catch {
-    return {};
-  }
+function read() {
+  try { return JSON.parse(fs.readFileSync(DB, 'utf8')); } catch { return {}; }
 }
-
-function writeDB(data) {
+function write(data) {
   fs.writeFileSync(DB, JSON.stringify(data, null, 2));
 }
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-
   const { longUrl } = req.body;
   if (!longUrl) return res.status(400).json({ error: 'URL wajib diisi' });
 
   const code = nanoid(6);
-  const db = readDB();
+  const db = read();
   db[code] = longUrl;
-  writeDB(db);
+  write(db);
 
   const shortUrl = `https://${req.headers.host}/${code}`;
   res.json({ shortUrl });
