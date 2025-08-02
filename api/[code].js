c53c6240c840ebbbefc2629d8403674bc89af40c
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:12:42 +0700

    Update [code].js

--- api/[code].js
+++ api/[code].js
@@ -1,21 +1,16 @@
-import fs from 'fs';
+import fs   from 'fs';
 import path from 'path';
 
 const DB = path.join(process.cwd(), 'db.json');
 
-function readDB() {
-  try {
-    return JSON.parse(fs.readFileSync(DB, 'utf8'));
-  } catch {
-    return {};
-  }
+function read() {
+  try { return JSON.parse(fs.readFileSync(DB, 'utf8')); } catch { return {}; }
 }
 
 export default async function handler(req, res) {
   const { code } = req.query;
-  const db = readDB();
+  const db = read();
   const longUrl = db[code];
-
   if (!longUrl) return res.status(404).send('URL tidak ditemukan');
   res.redirect(longUrl);
 }
