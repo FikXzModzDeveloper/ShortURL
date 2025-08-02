8903e11f6d3d08d719fc440f4923f3e8abfc4623
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:07:32 +0700

    Update [code].js

--- api/[code].js
+++ api/[code].js
@@ -1,8 +1,20 @@
-import { kv } from '@vercel/kv';
+import fs from 'fs';
+import path from 'path';
+
+const DB = path.join(process.cwd(), 'db.json');
+
+function readDB() {
+  try {
+    return JSON.parse(fs.readFileSync(DB, 'utf8'));
+  } catch {
+    return {};
+  }
+}
 
 export default async function handler(req, res) {
   const { code } = req.query;
-  const longUrl = await kv.get(`url:${code}`);
+  const db = readDB();
+  const longUrl = db[code];
 
   if (!longUrl) return res.status(404).send('URL tidak ditemukan');
   res.redirect(longUrl);
