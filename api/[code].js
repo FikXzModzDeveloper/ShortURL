e994af38c60130876f0a086f9f6d3d712677b0d5
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 08:28:13 +0700

    Create [code].js

---
+++ api/[code].js
@@ -0,0 +1,10 @@
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const longUrl = await kv.get(`url:${code}`);
+
+  if (!longUrl) return res.status(404).send('URL tidak ditemukan');
+
+  res.redirect(longUrl);
+}
