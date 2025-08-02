ce97cea324a96cbdcee8fcace6e68ce1d5477e9c
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:39:17 +0700

    Update [code].js

--- api/[code].js
+++ api/[code].js
@@ -1,12 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-
-const DB_PATH = path.resolve('./db.json');
-
 export default function handler(req, res) {
   const { code } = req.query;
-  const db = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8'));
-  const longUrl = db[code];
+  global.store = global.store || {};
+  const longUrl = global.store[code];
 
   if (!longUrl) return res.status(404).send('Not found');
   res.redirect(301, longUrl);
