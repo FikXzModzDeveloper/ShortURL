49b7fbf9d49c39c52945de148ff5168660ffcc3c
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-08-02 09:07:06 +0700

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -1,6 +1,20 @@
-// ganti require() dengan import
-import { kv } from '@vercel/kv';
 import { nanoid } from 'nanoid';
+import fs from 'fs';
+import path from 'path';
+
+const DB = path.join(process.cwd(), 'db.json');
+
+function readDB() {
+  try {
+    return JSON.parse(fs.readFileSync(DB, 'utf8'));
+  } catch {
+    return {};
+  }
+}
+
+function writeDB(data) {
+  fs.writeFileSync(DB, JSON.stringify(data, null, 2));
+}
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
@@ -8,9 +22,11 @@ export default async function handler(req, res) {
   const { longUrl } = req.body;
   if (!longUrl) return res.status(400).json({ error: 'URL wajib diisi' });
 
-  const shortCode = nanoid(6);
-  await kv.set(`url:${shortCode}`, longUrl);
+  const code = nanoid(6);
+  const db = readDB();
+  db[code] = longUrl;
+  writeDB(db);
 
-  const shortUrl = `https://${req.headers.host}/${shortCode}`;
+  const shortUrl = `https://${req.headers.host}/${code}`;
   res.json({ shortUrl });
 }
